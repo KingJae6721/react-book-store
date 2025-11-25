8bdf9a7e7ac83cfbc80414667d848f06b2ce9773
김영재 <gotjr44@naver.com>
2025-11-25 13:53:26 +0900

    React Layout

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import Layout from "./components/layout/Layout";
+import Home from "./pages/Home";
+import { GlobalStyle } from "./style/global";
+import { ThemeProvider } from "styled-components";
+import { ThemeName, getTheme } from "./style/theme";
+import ThemeSwitcher from "./components/header/ThemeSwitcher";
+import { useContext, useState } from "react";
+import { BookStoreThemeProvider, ThemeContext } from "./context/themeContext";
+
+function App() {
+  const { themeName, toggleTheme } = useContext(ThemeContext);
+  return (
+    <BookStoreThemeProvider>
+      <ThemeSwitcher />
+      <Layout>
+        <Home />
+      </Layout>
+    </BookStoreThemeProvider>
+
+  );
+}
+
+export default App;
